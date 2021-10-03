79e278a3c7fa41cfc7a51a95853515ff38b6b2f3
an-kashchuk <an.kashchuk@gmail.com>
2021-10-03 20:44:46 +0200

    Update example_fluorescent_xyz.m

--- Matlab/example_fluorescent_xyz.m
+++ Matlab/example_fluorescent_xyz.m
@@ -85,4 +85,4 @@ figure(53), plot(xplot,t), grid minor, grid on,hold on
             
 %% Alternatevily, the position of the particle can be determined with a single express function
 % Express calculation (uncomment to use)
-[x,y,z,Gthr]=LocalGradient.xyz_fluor_express(Im,R,thrsize,PositiveAngle,thrtype);
+% [x,y,z,Gthr]=LocalGradient.xyz_fluor_express(Im,R,thrsize,PositiveAngle,thrtype);
